6921cc6aa3de70be760888833211443481d98aef
Sukanya-gk72 <sukanya.kilabanur@gmail.com>
2024-12-21 13:53:48 +0530

    Add files via upload

--- Registration/regiscript.js
+++ Registration/regiscript.js
@@ -3,17 +3,4 @@ document.getElementById("applicationForm").addEventListener("submit", function (
     alert("Your application has been submitted!");
 });
 
-Submit_form.php
-
-<?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $name = htmlspecialchars($_POST['name']);
-    $email = htmlspecialchars($_POST['email']);
-    $phone = htmlspecialchars($_POST['phone']);
-    $qualification = htmlspecialchars($_POST['qualification']);
-    $message = htmlspecialchars($_POST['message']);
-
-    // You can customize this to save data in a database or send an email
-    echo "Thank you, $name! $email! $phone!
-    Your application has been received."
 
